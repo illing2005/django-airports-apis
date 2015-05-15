7a5849b61aa0e226d325e6742f322f88d8a22e1f
ilse <s.illing@gmx.de>
2015-05-16 01:50:43 +0200

    fixed typo

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-airport-apis',
+    name='django-airports-apis',
     version='0.0.1',
     packages=find_packages(exclude=['example']),
     include_package_data=True,
     license='MIT License',
     description='Airport and data for Django. Using different API Endpoints',
     long_description=README,
-    url='http://github.com/deinemudder',
+    url='https://github.com/illing2005/django-airports-apis',
     author='Sebastian Illing',
     author_email='s.illing@gmx.de',
     install_requires=[],    
